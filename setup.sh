65866439f04feec740d1dc3cf6946b875d1c215f
liutongxing <liutongxing@gmail.com>
2015-08-05 15:31:53 +0800

    add  -y parameter for apt-get to automate the script

--- setup.sh
+++ setup.sh
@@ -5,7 +5,7 @@ if [[ $EUID -ne 0 ]]; then
 fi
 
 # 1. install & config npm
-apt-get install npm
+apt-get -y install npm
 ln -s /usr/bin/nodejs /usr/bin/node
 npm config set registry http://registry.npm.taobao.org
 npm install -g npm
@@ -15,13 +15,13 @@ npm install -g yo generator-angular bower grunt-cli
 yo --version && bower --version && grunt --version
 
 # 3. install ruby 
-apt-get install rubygems-integration
+apt-get -y install rubygems-integration
 gem sources -l | grep http | xargs gem sources --remove
 gem sources -a https://ruby.taobao.org/
 gem sources -l
 
 # 4. yo angular
-apt-get install autoconf nasm git
+apt-get -y install autoconf nasm git
 
 echo Install completed. Now please input "yo angular" to start scaffolding a new project.
 echo Please be kindly reminded that it may cost a long time to download the dependencies.
